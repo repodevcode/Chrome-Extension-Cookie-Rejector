9d73d8ca408ed4b36ef5cb3cbbc0d9292b2ac353
repodevcode <grdfrlwn-s01@yahoo.com>
2024-12-08 13:46:48 -0500

    Made code more readable

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function rejectCookies(){
+    function cookieBannarManualSelect(){
+        document.getElementById("onetrust-pc-btn-handler").click()
+        for(let i = 0; i< document.getElementsByClassName("category-switch-handler").length; i++){
+            document.getElementsByClassName("category-switch-handler")[i].click()
+        }
+        document.getElementsByClassName("save-preference-btn-handler onetrust-close-btn-handler")[0].click()
+    }
     try{
         document.getElementsByClassName("ot-pc-refuse-all-handler")[0].click()
     }
@@ -7,11 +14,7 @@ function rejectCookies(){
             document.getElementById("onetrust-reject-all-handler").click()
         }
         catch(err){
-            document.getElementById("onetrust-pc-btn-handler").click()
-            for(let i = 0; i< document.getElementsByClassName("category-switch-handler").length; i++){
-                document.getElementsByClassName("category-switch-handler")[i].click()
-            }
-            document.getElementsByClassName("save-preference-btn-handler onetrust-close-btn-handler")[0].click()
+            cookieBannarManualSelect()
         }
     }
 }
