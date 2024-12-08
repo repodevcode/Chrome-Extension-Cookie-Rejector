3b3f32583261c2359e94cd96f92fd88c2c3b764c
repodevcode <grdfrlwn-s01@yahoo.com>
2024-12-08 13:45:39 -0500

    Added another cookie banner type

--- script.js
+++ script.js
@@ -3,11 +3,19 @@ function rejectCookies(){
         document.getElementsByClassName("ot-pc-refuse-all-handler")[0].click()
     }
     catch(err){
-        document.getElementById("onetrust-reject-all-handler").click()
+        try{
+            document.getElementById("onetrust-reject-all-handler").click()
+        }
+        catch(err){
+            document.getElementById("onetrust-pc-btn-handler").click()
+            for(let i = 0; i< document.getElementsByClassName("category-switch-handler").length; i++){
+                document.getElementsByClassName("category-switch-handler")[i].click()
+            }
+            document.getElementsByClassName("save-preference-btn-handler onetrust-close-btn-handler")[0].click()
+        }
     }
 }
 setTimeout(rejectCookies, 5000);
-setTimeout(rejectCookies, 10000);
-setTimeout(rejectCookies, 15000);
 
 
+//TODO: optimize program by using lists to save IDs
