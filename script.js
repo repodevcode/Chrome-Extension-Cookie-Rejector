b6be1e68611790a53333950a87c884e870c42736
repodevcode <grdfrlwn-s01@yahoo.com>
2024-12-08 11:03:20 -0500

    Added another OneTrust cookie to reject

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 try{
     document.getElementsByClassName("ot-pc-refuse-all-handler")[0].click()
+    document.getElementById("onetrust-reject-all-handler").click()
 }
 catch(err){
     console.log("No cookies to reject")
 }
+
+
+
