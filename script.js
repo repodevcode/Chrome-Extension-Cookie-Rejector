34002f74412b769a24548a825892502296102511
repodevcode <grdfrlwn-s01@yahoo.com>
2024-12-08 19:52:38 -0500

    Revert "Merge branch 'main' of https://github.com/repodevcode/Chrome-Extension-Cookie-Rejector"
    This reverts commit f4fabdc87550a65b2e0217a560478f9b12599428, reversing
    changes made to e34c0a9e533bc777c03aafe439dfe7834472c4c2.

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
 function rejectCookies(){
-    if(location.href.includes("ok.ru") == true){
-        document.getElementsByClassName("button-pro __solid-white cb_setup js-cb_setup")[0].click()
-        document.getElementsByName("button_save_choice")[0].click()
-    }
-    else if(location.href.includes("10minutemail.net") == true){
-        document.getElementsByClassName("fc-button-label")[1].click()
-        for (let i = 0; i<document.getElementsByClassName("fc-slider-el").length; i++){
-            document.getElementsByClassName("fc-slider-el")[i].click()
+    function cookieBannarManualSelect(){
+        document.getElementById("onetrust-pc-btn-handler").click()
+        for(let i = 0; i< document.getElementsByClassName("category-switch-handler").length; i++){
+            document.getElementsByClassName("category-switch-handler")[i].click()
         }
+        document.getElementsByClassName("save-preference-btn-handler onetrust-close-btn-handler")[0].click()
     }
-    else{
-        function cookieBannarManualSelect(){
-            document.getElementById("onetrust-pc-btn-handler").click()
-            for(let i = 0; i< document.getElementsByClassName("category-switch-handler").length; i++){
-                document.getElementsByClassName("category-switch-handler")[i].click()
-            }
-            document.getElementsByClassName("save-preference-btn-handler onetrust-close-btn-handler")[0].click()
-        }
+    try{
+        document.getElementsByClassName("ot-pc-refuse-all-handler")[0].click()
+    }
+    catch(err){
         try{
-            document.getElementsByClassName("ot-pc-refuse-all-handler")[0].click()
+            document.getElementById("onetrust-reject-all-handler").click()
         }
         catch(err){
-            try{
-                document.getElementById("onetrust-reject-all-handler").click()
-            }
-            catch(err){
-                cookieBannarManualSelect()
-            }
+            cookieBannarManualSelect()
         }
     }
 }
